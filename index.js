7e55a5733c71bccfb977c87d5901d79e1885ac84
hoang <vinhyenvodoi98@gmail.com>
2020-04-21 11:19:00 +0700

    update docker docker-compose

--- index.js
+++ index.js
@@ -15,48 +15,54 @@ db.collection_name.find(): dua ra tat ca data trong collection.
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
 const connectMongo = require('connect-mongo');
-const connectFlash = require("connect-flash");
-const edge = require("edge.js");
+const connectFlash = require('connect-flash');
+const edge = require('edge.js');
 
 const app = new express();
 
-app.use(expressSession({
-    secret: 'secret'
-}));
+app.use(
+  expressSession({
+    secret: 'secret',
+  })
+);
 
 const Post = require('./database/models/Post');
-const auth = require("./middleware/auth");
-const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
-const logoutController = require("./controllers/logout");
+const auth = require('./middleware/auth');
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated');
+const logoutController = require('./controllers/logout');
 
-const createPostController = require('./controllers/createPost')
-const homePageController = require('./controllers/homePage')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
-const createUserController = require("./controllers/createUser"); 
+const createPostController = require('./controllers/createPost');
+const homePageController = require('./controllers/homePage');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const createUserController = require('./controllers/createUser');
 const storeUserController = require('./controllers/storeUser');
-const loginController = require("./controllers/login");
+const loginController = require('./controllers/login');
 const loginUserController = require('./controllers/loginUser');
 
-mongoose.connect('mongodb://localhost:27017/blog', 
-                    { useNewUrlParser: true, 
-                    useUnifiedTopology: true, 
-                    useCreateIndex: true,})
-    .then(() => 'You are now connected to Mongo!')
-    .catch(err => console.error('Something went wrong', err))
+mongoose
+  .connect('mongodb://mongo:27017/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => 'You are now connected to Mongo!')
+  .catch((err) => console.error('Something went wrong', err));
 
 const mongoStore = connectMongo(expressSession);
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: 'secret',
     store: new mongoStore({
-        mongooseConnection: mongoose.connection
-    })
-}));
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+);
 
 app.use(fileUpload());
 app.use(connectFlash());
@@ -65,33 +71,34 @@ app.use(expressEdge.engine);
 app.set('views', __dirname + '/views');
 
 app.use('*', (req, res, next) => {
-    edge.global('auth', req.session.userId)
-    next()
+  edge.global('auth', req.session.userId);
+  next();
 });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const storePost = require('./middleware/storePost')
-app.use('/posts/store', storePost)
+const storePost = require('./middleware/storePost');
+app.use('/posts/store', storePost);
 
-app.get("/", homePageController);
-app.get("/post/:id", getPostController);
-app.get("/posts/new",auth, createPostController);
-app.post("/posts/store", storePostController);
-app.get('/auth/login',  redirectIfAuthenticated,loginController);
-app.post('/users/login',  redirectIfAuthenticated,loginUserController);
-app.get("/auth/register",  redirectIfAuthenticated,createUserController);
-app.post("/users/register",  redirectIfAuthenticated,storeUserController);
-app.get("/auth/logout", redirectIfAuthenticated, logoutController);
-
+app.get('/', homePageController);
+app.get('/post/:id', getPostController);
+app.get('/posts/new', auth, createPostController);
+app.post('/posts/store', storePostController);
+app.get('/auth/login', redirectIfAuthenticated, loginController);
+app.post('/users/login', redirectIfAuthenticated, loginUserController);
+app.get('/auth/register', redirectIfAuthenticated, createUserController);
+app.post('/users/register', redirectIfAuthenticated, storeUserController);
+app.get('/auth/logout', redirectIfAuthenticated, logoutController);
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
 });
- 
+
 app.listen(3000, () => {
-    console.log('App listening on port 3000')
+  console.log('App listening on port 3000');
 });
